d0d53a91b33eed7b2d1dcfa0d2526870882ad580
Alex Kenny <33143013+AlexKenny6@users.noreply.github.com>
2019-02-27 19:33:41 -0800

    Add files via upload

---
+++ Code/footer.php
@@ -0,0 +1,23 @@
+	<footer class="footer">
+        <div class="row">
+            <div class="col-1-of-2">
+                <div class="footer__navigation">
+                    <ul class="footer__list">
+                        <li class="footer__item"><a href="../../../index.php" class="footer__link">Home</a></li>
+                        <li class="footer__item"><a href="../../../thePictures.php" class="footer__link">Pictures</a></li>
+                        <li class="footer__item"><a href="../../../login.php" class="footer__link">Log In</a></li>
+                        <!-- <li class="footer__item"><a href="../../../contact.php" class="footer__link">Contact</a></li> --> 
+                    </ul>
+                </div>
+            </div>
+            <div class="col-1-of-2">
+                <p class="footer__copyright">
+					&copy; 2019, Lucy Photos, All rights reserved.<br>
+                    Built by <a href="https://pacwestsoftware.com/index.php" class="footer__link">Pacwest Software Development LLC</a>. <br> Copyright &copy; Pacwest Software Development LLC, All Rights Reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
+
+    </body>
+</html>
