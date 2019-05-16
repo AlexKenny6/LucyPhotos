407300f077bf1d0c9f5ead6a6c68ef41846cd817
Alex Kenny <33143013+AlexKenny6@users.noreply.github.com>
2019-05-15 22:10:09 -0700

    Updated Login Code

--- Code/Public Code/login.php
+++ Code/Public Code/login.php
@@ -8,16 +8,10 @@
             </h1>
             <a href="thePictures.php" class="btn btn--white btn--animated">Discover The Pictures!</a>
         </div>
-    <br><br><br><br><br>
-    <br><br><br><br><br>
-    <br><br><br><br><br>
-    <br><br><br><br><br>
         <h2 class="u-center-text bg-warning"><?php display_message(); ?></h2>
         <div class="row">
             <div class="form__form">
                 <hr>
-                <br>
-                <br>
                 <br>
                 <center>
                     <form class="form" action="" method="post" enctype="multipart/form-data">
@@ -27,8 +21,6 @@
                         <div class="form_group">
                             <label for=""><h1>Username</h1><br><input type="text" name="username" class="form__input u-center-text"></label>
                         </div>
-                        <br>
-                        <br>
                         <br>
                         <div class="form_group">
                             <label for="password"><h1>Password</h1><br><input type="password" name="password" class="form__input u-center-text"></label>
@@ -43,10 +35,5 @@
         </div>
     </section>
     <br>
-    <br> 
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+
 <?php include(TEMPLATE_FRONT . DS . "footer.php");  ?>
